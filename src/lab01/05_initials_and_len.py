21c671b00b2a2b790eb1edcc66f4f7c1f6e34f9c
slavyakhin <slavyakhin@gmail.com>
2026-09-18 15:25:12 +0300

    fix(lab01): 05 add period

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -3,7 +3,7 @@ def main():
     if (len(nameList) == 0):
         raise ValueError("Name is required")
 
-    print(f'Инициалы: {"".join(map(lambda word: word[0], nameList))}')
+    print(f'Инициалы: {"".join(map(lambda word: word[0], nameList))}.')
     print(f'Длина (символов): {sum(map(len, nameList)) + len(nameList) - 1}') # usually 2 additional spaces
 
 if __name__ == "__main__":
